6e610acd7f1bbb2dee51e137495fa80aa2fd46e2
LiangKoya <[y_koyanaka2@yahoo.co.jp]>
2024-08-20 20:29:50 +0800

    added links which jump to top-page afer 5sec of login

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # ForUs
+https://qiita.com/whw3312/items/94a2bdf632ef77555579 :  js evnet 一覧 addEventListener
+https://javascript.programmer-reference.com/js-dom-inntertext/ :  .innerTextの例
+https://www.javadrive.jp/javascript/form/index1.html　：　テキストボックスの値の取得
+
--- javascript.js
+++ javascript.js
@@ -24,6 +24,10 @@ pass.addEventListener("focus", ()=>{
     pass.value = ""
 })
 
+login_button.addEventListener("click", ()=>{
+    hint.innerText=""
+});
+
 login_button.addEventListener("click", disp_login_result);
 
 function disp_login_result(){
@@ -31,6 +35,9 @@ function disp_login_result(){
     if (user_name.value == "张紫嫣"){
         if (input_pass == 20010805){
             pass_result.innerText = "Finally you came here!"
+            window.setTimeout(()=>{
+                window.location.href="top4ziyan.html"
+            },3000)
         }
         else{
             alert("Babe, that\'s wrong. Remember THE DATE!")
@@ -39,6 +46,9 @@ function disp_login_result(){
     else if(user_name.value == "郭强"){
         if (input_pass == 19981129){
             pass_result.innerText = "Login successed!"
+            window.setTimeout(()=>{
+                window.location.href="top.html"
+            },3000)
     }
         else{
             alert("Wrong! Try again!")
